346d87ef238a7859dd497f238b86d12677885e96
vitor-costa <v2costa@gmail.com>
2016-03-03 00:14:57 -0300

    Get port from environment variable

--- tornado-ws.py
+++ tornado-ws.py
@@ -2,7 +2,9 @@ from tornado.wsgi import WSGIContainer
 from tornado.httpserver import HTTPServer
 from tornado.ioloop import IOLoop
 from app import app
+import os
 
 http_server = HTTPServer(WSGIContainer(app))
-http_server.listen(5000)
+port = int(os.environ.get("PORT", 5000))
+http_server.listen(port)
 IOLoop.instance().start()
